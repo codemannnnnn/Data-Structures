76d4145bdb909ea87c3e5d0dbf114d098852f545
codemannnnnn <codybranson@gmail.com>
2020-07-13 23:03:13 -0600

    mvp

---
+++ examples/teststack.py
@@ -0,0 +1,24 @@
+class Cody:
+    def __init__(self):
+        self.storage = []
+
+
+    def add(self, value):
+        self.storage.append(value)
+
+
+    def len(self, value):
+        self.storage = len(self.storage)
+
+
+
+
+
+
+
+
+x = Cody()
+
+x.add(4)
+
+print(x)
--- stack/stack.py
+++ stack/stack.py
@@ -10,22 +10,52 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when
    implementing a Stack?
 """
+
+
+from singly_linked_list import LinkedList
+
+
+
+# Linked List
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
         return self.size
 
     def push(self, value):
-        self.storage.append(value)
-        self.size = len(self.storage)
+        self.storage.add_to_tail(value)
+        self.size += 1
+        return value
 
     def pop(self):
-        if self.size == 0:
-            return None
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_tail()
         else:
-            popp = self.storage.pop()
-            self.size = len(self.storage)
-            return popp
+            return None
+
+
+
+# Array
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+#
+#     def __len__(self):
+#         return self.size
+#
+#     def push(self, value):
+#         self.storage.append(value)
+#         self.size = len(self.storage)
+#
+#     def pop(self):
+#         if self.size == 0:
+#             return None
+#         else:
+#             popp = self.storage.pop()
+#             self.size = len(self.storage)
+#             return popp
